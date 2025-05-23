1e65f978cbda2e56d1d4d02c63011fb4b312265e
ScottShadow <rudaseswascottmc@gmail.com>
2025-05-23 02:16:37 +0200

    cookie fix

--- server/auth.ts
+++ server/auth.ts
@@ -34,7 +34,7 @@ export function setupAuth(app: express.Express) {
       saveUninitialized: false,
       cookie: {
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-        secure: process.env.NODE_ENV === "production",
+        secure: false,
       },
     })
   );
